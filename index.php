fc47841bc9f4793af36474379f336d21c7c27486
jan <ferri2903@gmail.com>
2025-01-14 16:04:46 +0100

    Afegir un css i modificació en el index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "controller/controller.php";
+// require_once "controller/controller.php";
 
 $msgError = "";
 $errorBox = "";
@@ -11,20 +11,23 @@ $errorBox = "";
 <html lang="es">
 
 <head>
-  <title>Login | Cetisi</title>
+  <title>Login | EduFòrum</title>
   <meta charset="utf-8">
   <meta name="author" content="Cetisi">
   <meta name="description" content="Programming courses website by Cetisi">
   <meta name="keywords" content="programming, courses, learn, education, web, development">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="./img/logo-white.png">
-
+  <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet"> 
+  <link rel="stylesheet" href="./css/index.css">
+  <!-- <link rel="stylesheet" href="./css/output.css">
+  <link rel="stylesheet" href="./css/common.css"> -->
 </head>
 <!--pero la queria tanto que daba igual por-->
 <body id="screen">
     <section class="text-animation-box">
         <div>
-        <h1 class="typeHeader">Welcome back!</h1> 
+        <h1 class="typeHeader">Welcome back! EduFòrum</h1> 
         <p class="typeText"></p>
         </div>
     </section>
@@ -36,7 +39,7 @@ $errorBox = "";
         <form class="reset-password-form inactive" action="<?php htmlspecialchars($_SERVER["REQUEST_METHOD"]) ?>" method="POST">
             <div class="input-box" id="input-email">
                 <label for="email"><ion-icon name="person-outline"></ion-icon></label>
-                <input type="text" id="resetPassMail" name="resetPassMail" required="true" placeholder="">
+                <input type="text" id="resetPassMail" name="resetPassMail" required="true" placeholder="Introduiex el email">
                 <span>Email</span>
                 <p id="userError" class="inactive"></p>
             </div>
@@ -47,14 +50,14 @@ $errorBox = "";
         <form class="login-form" action="<?php htmlspecialchars($_SERVER["REQUEST_METHOD"]) ?>" method="POST">
             <div class="input-box" id="input-usr">
                 <label for="usr"><ion-icon name="person-outline"></ion-icon></label>
-                <input type="text" id="usr" name="user" required="true" placeholder="">
-                <span>User / Email</span>
+                <input type="text" id="usr" name="user" required="true" placeholder="Introduiex el usuari">
+                <span>User / Mail</span>
                 <p id="userError" class="inactive"></p>
             </div>
 
             <div class="input-box" id="input-pwd">
                 <label for="pswd"><ion-icon name="lock-closed-outline"></ion-icon></label>
-                <input type="password" id="pswd" name="password" required placeholder="">
+                <input type="password" id="pswd" name="password" required placeholder="Introduiex el password">
                 <span>Password</span>
                 <p id="error" class="inactive"></p>
             </div>
@@ -63,21 +66,21 @@ $errorBox = "";
         </form>
 
         <div class="forgot-password">
-            <p id="toggle-area-text">Forgot password?</p>
+            <!--<p id="toggle-area-text">Forgot password?</p>
             <strong id="toggle-form">Click here.</strong>
-        </div>
+        </div>-->
 
         <div class="change-form">
-            <p>Don't have an account?</p>
-            <a href="./view/register.php">Sign Up</a>
+            <p>No tens compte?</p>
+            <a href="./view/register.php">Registra't</a>
         </div>
   </section>
   
-  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <!-- <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
   <script src="./js/typing.js"></script>
   <script src="./js/background.js"></script>
-  <script src="./js/modal.js"></script>
+  <script src="./js/modal.js"></script> -->
 </body>
 
 </html>
